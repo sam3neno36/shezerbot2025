2547c3dff43b5f832ba43e526959ce52760b375d
sam3neno36 <53592309+sam3neno36@users.noreply.github.com>
2025-06-09 14:48:25 +0300

    edit db.py

--- db.py
+++ db.py
@@ -2,7 +2,7 @@
 import asyncpg
 import ssl
 
-DB_URL = "postgres://your_user:your_password@your_host:your_port/your_db"
+DB_URL = "postgresql://shezer_db_user:zk5eDxVZorkemREPa2oUDMyC7VEtzezR@dpg-d13cfb49c44c7395m4u0-a.oregon-postgres.render.com/shezer_db"
 
 async def connect_db():
     try:
